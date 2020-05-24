fb9036126030d5a778029acc9106f0baf6843bc7
Ap3teus <asver37@gmail.com>
2020-05-24 23:31:30 +0300

    feat: better sqitch script

--- database/sqitch.sh
+++ database/sqitch.sh
@@ -1,1 +1,19 @@
-sqitch deploy db:pg:$POSTGRES_DB
+#!/bin/bash
+
+cd $(dirname $0)
+ENV=$1
+if [ "$ENV" != "prod" -a "$ENV" != "dev" ]
+then
+  echo "Invalid environment $ENV"
+  set -e
+  false
+fi
+
+DB="db:pg:$POSTGRES_DB"
+
+if [ "$ENV" = "prod" ]
+then
+  DB=$PROD_DB
+fi
+
+sqitch "${@:2}" $POSTGRES_DB
