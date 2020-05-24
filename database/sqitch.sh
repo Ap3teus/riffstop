af354645fc503df6a2584f53224fa6fecda194fe
Ap3teus <asver37@gmail.com>
2020-05-24 23:47:04 +0300

    feat: ask confirmation for actions targeting prod

--- database/sqitch.sh
+++ database/sqitch.sh
@@ -14,6 +14,13 @@ DB="db:pg:$POSTGRES_DB"
 if [ "$ENV" = "prod" ]
 then
   DB=$PROD_DB
+  read -p "Are you sure? [y/N]: "  -r
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    sqitch "${@:2}" $DB
+  else
+    echo "Aborting"
+  fi
+else
+  sqitch "${@:2}" $DB
 fi
-
-sqitch "${@:2}" $POSTGRES_DB
