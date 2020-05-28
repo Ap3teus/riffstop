f998870f6a63c278a7b7ffcf0bf8331e1621e380
Ap3teus <asver37@gmail.com>
2020-05-28 22:21:33 +0300

    fix: remove console.log

--- functions/src/hello.ts
+++ functions/src/hello.ts
@@ -2,7 +2,6 @@ import { Handler } from 'aws-lambda';
 import { db } from './common/db';
 
 export const handler: Handler = async (event, context) => {
-  console.log('asd');
   const res = await db.any(`select * from foo;`);
 
   return {
