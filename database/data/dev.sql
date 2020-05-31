d4ded153fa1915f131ef4e2e61b105786fba93ed
Ap3teus <asver37@gmail.com>
2020-05-31 23:05:28 +0300

    feat: missing song-scripts (revert and deploy)

--- database/data/dev.sql
+++ database/data/dev.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-INSERT INTO account (id, name, email) values ('91515035-adc7-405b-ad4d-a11fed20a074', 'bar');
+INSERT INTO account (id, name, email, password_hash) values ('91515035-adc7-405b-ad4d-a11fed20a074', 'ape', 'ape@localhost', '');
 
 COMMIT;
